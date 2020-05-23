cefdfe2e9b5d00f3e499be6c114bce791c871d69
Agustin <agusstinbj@gmail.com>
2020-05-23 19:46:15 -0300

    GRUPO - 79 Se agrego verificar usuario registrado y logeado

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -24,5 +24,29 @@ class LoginController
     header('Location: '.login);
   }
 
+  function chekedLogin() {
+    session_start();
+    if(!issset($_SESSION['ID_USER'])) {
+      header('Location'. LOGIN);
+      die();
+    }
+  }
+
+  function verifyUser() {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $user = this->model->getByUsername($username);
+    if(!empty($user) && password_verify($password, $user->password)) {
+      session_start();
+      $_SESSION['ID_USER'] = $user->id;
+      $_SESSION['USERNAME'] = $user->username;
+      
+      header('Location ver');
+     } else {
+       $this->view->showLogin("Login incorrecto");
+     }
+  }
+
 }
 ?>
